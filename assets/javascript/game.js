2c4ed4a6407cc353106702d903cfb53ebeb1b98e
pmmiv <pmmilamiv@gmail.com>
2018-01-22 16:09:55 -0600

    some assets and initalization

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+// player selects a character
+
+
+// other characters become enemies
+
+
+// player selects an enemy to attack
+
+
+// player attacks active enemy by clicking a button
+
+
+// text displays the damage of the attack and counter attack. Health values change.
+
+
+// active enemy disappears and the two remaining enemies are available to attack. Player health remains the same after the battle.
+
+
+// player selects one of the remaining enemies to attack, and repeats fighting sequence until he is out of enemies or health.
+
+
+// player character health reaches 0. Player loses and has the ability to reset the game.
+
+
+// player runs out of enemies and a win message displays with a reset button
+
+
+// ***** Characters *****
+// Frodo - 120 health 8 + 8x Damage
+// Gollum - 100 health 5 damage
+// Boromir - 150 health 20 damage
+// Sauron - 180 health 25 damage
