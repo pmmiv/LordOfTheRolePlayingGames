08b7ffa3263ac738d190cf682b92e579e3e5e108
PMMIV <pmmilamiv@gmail.com>
2018-01-22 23:13:19 -0600

    Completed initial UI, and began coding the logic. Still needs battle sequence.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,72 @@
-// player selects a character
+var frodo = {
+	"name" : "frodo",
+	"health" : 120
+}
+var gollum = {
+	"name" : "gollum",
+	"health" : 100,
+	"damage" : 5
+}
+var boromir = {
+	"name" : "boromir",
+	"health" : 150,
+	"damage" : 20
+} 
+var sauron = {
+	"name" : "sauron",
+	"health" : 180,
+	"damage" : 25
+}
+var fighters = [frodo, gollum, boromir, sauron];
+var heroChosen ;
+var enemyChosen ; // maybe not needed? 
 
+$(document).ready(function(){
+	// creates fighter buttons
+	for (i = 0; i < fighters.length; i++) {
+		var fighterCard = $("<button>");
+		fighterCard.addClass("fighter");
+		fighterCard.attr("id", fighters[i].name);
+		fighterCard.text(fighters[i].name + ": " + fighters[i].health + " HP");
+		$("#bench").append(fighterCard);
+		$("#" + fighters[i].name).css("background-image", "url('assets/images/" + fighters[i].name + ".jpg')");
+	};
 
-// other characters become enemies
+	$(".fighter").on("click", function(){
+		if (heroChosen === undefined) {
+			$(".battleground").append(this);
+			$("#m1").html("You selected:");
+			$("#m2").html("Excellent Choice.");			
+			$("#m3").html("Select your foe.");			
+			$(this).addClass("hero");
+			heroChosen = $(this).attr("id");
+		}
+		else {
+			$(".hero").css("margin-right", "80px");
+			$(".battleground").append("<span id='vs'>Versus</span>");
+			$(".battleground").append(this);
+			$("#m1").html("Begin!");
+			$("#m2").html("<button id='action'>Attack!</button>");
+			$(this).addClass("activeEnemy");
+			$(".activeEnemy").css("margin-left", "80px")
+			enemyChosen = $(this).attr("id");
+			$(".container2").css("display", "none")
+		}
+	});
 
+	// $("#action").on("click", function(){
+		// if (heroChosen === "frodo") {
+// math for frodo's damage
+// subtract hero damage from enemy health
+// subtract enemy damage from hero health
+// target enemy card and print new hp text
+// if enemy health is <1 and enemies remain set activeEnemy to undefined, and bring $(".container2") back
+// if enemy health is <1 and no enemies remain display win message and change button to reset the game.
+// target hero card and print new hp text
+		// };
+		// else {
+// do everything done for frodo except hero damage is set in the object
+		// };
 
-// player selects an enemy to attack
-
-
-// player attacks active enemy by clicking a button
-
-
-// text displays the damage of the attack and counter attack. Health values change.
-
-
-// active enemy disappears and the two remaining enemies are available to attack. Player health remains the same after the battle.
-
-
-// player selects one of the remaining enemies to attack, and repeats fighting sequence until he is out of enemies or health.
-
-
-// player character health reaches 0. Player loses and has the ability to reset the game.
-
-
-// player runs out of enemies and a win message displays with a reset button
-
-
-// ***** Characters *****
-// Frodo - 120 health 8 + 8x Damage
-// Gollum - 100 health 5 damage
-// Boromir - 150 health 20 damage
-// Sauron - 180 health 25 damage
+	// });
+});
