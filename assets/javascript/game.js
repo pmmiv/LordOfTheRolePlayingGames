a0ebdd31e7d3aa633972f473290706a0beae50d7
PMMIV <pmmilamiv@gmail.com>
2018-01-24 23:59:56 -0600

    battle logic complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,30 +22,51 @@ var heroChosen ;
 var enemyChosen ; // maybe not needed? 
 var attackCounter = 0;
 var frodoDamage ;
+var enemiesRemaining = 3;
+var gameOver = false;
 
 function attack () {
 	var heroDamage, enemyDamage;
-	if (heroChosen === frodo || enemyChosen === frodo) {
+	if (gameOver) {
+		location.reload();
+	}
+	else if (heroChosen === frodo || enemyChosen === frodo) {
 		attackCounter++;
 		frodoDamage = attackCounter * 8;
 		if (heroChosen === frodo) {
-			enemyChosen.health = enemyChosen.health - frodoDamage; 
-			heroChosen.health = heroChosen.health - enemyChosen.damage;
 			heroChosen.damage = frodoDamage;
 		} else {
-			enemyChosen.health = enemyChosen.health - heroChosen.damage;
-			heroChosen.health = heroChosen.health - frodoDamage;
 			enemyChosen.damage = frodoDamage;
 		}
-	console.log(frodoDamage);
 	}
-	else {
-		enemyChosen.health = enemyChosen.health - heroChosen.damage;
+	enemyChosen.health = enemyChosen.health - heroChosen.damage;
+	if (enemyChosen.health > 0) {
 		heroChosen.health = heroChosen.health - enemyChosen.damage;
-	}
+	} 
 	$(".activeEnemy").text(enemyChosen.name + ": " + enemyChosen.health + " HP");
 	$(".hero").text(heroChosen.name + ": " + heroChosen.health + " HP");
 	$("#m1").html("<p>"+heroChosen.name+" attacked "+enemyChosen.name+" for "+heroChosen.damage+" damage.</p><p>"+enemyChosen.name+" attacked "+heroChosen.name+" for "+enemyChosen.damage+" damage.</p>");
+	if (heroChosen.health <= 0) {
+		$("#m1").html("You lose.");
+		$("#action").text("Play Again");
+		$(".hero").css("display", "none");
+		$(".vs").css("display", "none")
+		gameOver = true;
+	} else if (enemyChosen.health <= 0 && enemiesRemaining === 1) {
+		$("#action").text("Play Again");
+		$("#m1").html("You've won!");
+		$(".activeEnemy").css("display", "none");
+		$(".vs").css("display", "none")
+		gameOver = true;
+	} else if (enemyChosen.health <= 0) {
+		enemyChosen = 0;
+		$("#action").css("display", "none");
+		$(".container2").css("display", "block");
+		$("#m1").html("Choose your next opponent!");
+		$(".activeEnemy").css("display", "none");
+		$(".vs").css("display", "none")
+		enemiesRemaining--;
+	}
 };
 
 
@@ -71,14 +92,14 @@ $(document).ready(function(){
 			heroChosen = $(this).data("data");
 		}
 		else {
-			$(".hero").css("margin-right", "80px");
-			$(".battleground").append("<span id='vs'>Versus</span>");
+			$(".hero").css("margin", "20px 80px");
+			$(".battleground").append("<span class='vs'>Versus</span>");
 			$(".battleground").append(this);
 			$("#m1").html("Begin!");
 			$("#m2").html("<button id='action'>Attack!</button>");
 			$('#action').click(attack);
 			$(this).addClass("activeEnemy");
-			$(".activeEnemy").css("margin-left", "80px")
+			$(".activeEnemy").css("margin", "20px 80px")
 			enemyChosen = $(this).data("data");
 			$(".container2").css("display", "none")
 		}
